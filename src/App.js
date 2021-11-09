09193df8d38d31ada0eb5ed0bd8fd1733b9778c5
bora001 <bora.vancouver@gmail.com>
2021-11-09 14:59:11 -0800

    redux added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { createStore } from "redux";
+
 function App() {
+  function appReducer(state, action) {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  }
+
+  let store = createStore(appReducer);
+  store.subscribe(() => console.log(store.getState()));
+
   return <div></div>;
 }
 
