e59db3ec5c0e420a605638f973a02e6e1bda1631
bora001 <bora.vancouver@gmail.com>
2021-11-11 12:13:03 -0800

    setting format

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-import "./Redux";
+import { plus, minus } from "./Redux";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "130px",
+        }}
+      >
+        <button onClick={plus}>➕</button>
+        <p>0</p>
+        <button onClick={minus}>➖</button>
+      </div>
+    </div>
+  );
 }
 export default App;
