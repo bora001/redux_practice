029484c044d0d3cbdfaa420f2968ecf78ba1ad25
bora001 <bora.vancouver@gmail.com>
2021-11-11 11:56:21 -0800

    create store at index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./Redux";
-
+import { createStore } from "redux";
+import reducer from "./Redux";
+// store
+const store = createStore(reducer);
+console.log(store.getState());
 ReactDOM.render(<App />, document.getElementById("root"));
